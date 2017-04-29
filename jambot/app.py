12e33cd04559f360519fa42a35cc92504582d3c3
Ubuntu <ubuntu@ip-172-31-24-107.us-west-2.compute.internal>
2017-04-29 21:37:59 +0000

    Start Fresh

---
+++ jambot/app.py
@@ -0,0 +1,37 @@
+import sys
+import json
+from flask import Flask
+from flask import render_template
+from flask import jsonify
+from cStringIO import StringIO
+from verse import Verse
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("jambot.html")
+
+@app.route('/create')
+def create():
+    song = Verse()
+    old_stdout = sys.stdout
+    sys.stdout = mystdout = StringIO()
+    song.print_verse()
+    sys.stdout = old_stdout
+    print_song = jsonify(mystdout.getvalue()) 
+    verse = {"key": song.key, "scale": song.scale,
+             "melody": song.note_arrangement, "beats": song.beat_arrangement}
+    return jsonify(verse=verse, notation=mystdout.getvalue())
+        
+@app.route('/play', methods = ['POST'])
+def play():
+    data = json.loads(request.form.get("verse"))
+    song = Verse(4, data["key"], data["scale"], "placeholder", "placeholder",
+                 data["melody"], data["beats"])
+    song.play()    
+    return jsonify(verse=data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ jambot/application.py
@@ -0,0 +1,43 @@
+import sys
+import json
+from flask import Flask
+from flask import render_template
+from flask import jsonify
+from cStringIO import StringIO
+from verse import Verse
+from flask import request
+from flask import send_file
+application = Flask(__name__)
+
+@application.route('/')
+def index():
+    return render_template("jambot.html")
+
+@application.route('/create')
+def create():
+    song = Verse()
+    old_stdout = sys.stdout
+    sys.stdout = mystdout = StringIO()
+    song.print_verse()
+    sys.stdout = old_stdout
+    print_song = jsonify(mystdout.getvalue()) 
+    verse = {"key": song.key, "scale": song.scale,
+             "melody": song.note_arrangement, "beats": song.beat_arrangement}
+    return jsonify(verse=verse, notation=mystdout.getvalue())
+
+@application.route('/play', methods = ['POST'])
+def play():
+    data = json.loads(request.form.get("verse")) 
+    song = Verse(4, data["key"], data["scale"], "placeholder", "placeholder",
+                 data["melody"], data["beats"])
+    return jsonify(url=song.wavefile)
+#    return send_file(path_to_file, 
+ #                    mimetype="audio/wav", 
+  #                   as_attachment=True, 
+   #                  attachment_filename="output.wav")
+#   song.play()    
+#    return jsonify(verse=data)
+
+
+if __name__ == "__main__":
+    application.run(debug=True)
