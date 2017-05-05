5993dd2970d60bc149b2d7572677ee80e3deb86f
BK <kluthr@ip-172-31-20-133.us-west-2.compute.internal>
2017-05-05 01:08:35 +0000

    Add error page

---
+++ jambot/static/jambot2.js
@@ -0,0 +1,43 @@
+$(document).ready( function(){
+  Typed.new(".writing", {
+    strings: ["> Hi, I'm Jambot!<br>> Press play and I will write you a song..."],
+    typeSpeed: 0,
+    showCursor: false
+  });
+
+$.ajax({
+  url: "/create", 
+  cache: false, 
+  success: function(data) {
+    alert(data.url);
+    alert(data.notation);
+    var soundFile = document.createElement("audio");
+    var src = document.createElement("source");
+    src.src = "";
+    soundFile.appendChild(src);
+    soundFile.load();
+    soundFile.volume = 0.000000;
+          
+    function play() {
+      soundFile.currentTime = 0.01;
+      soundFile.volume = .25;
+      setTimeout(function(){soundFile.play();},1);
+    }
+    src.src = data.url;
+    soundFile.appendChild(src);
+    $('#playbutton').on('click', function() {
+      play()
+      Typed.new(".song", {
+        strings: ["> Here's your song!<br>> " + data.notation + "<br>> I hope you like it!<br>>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<3 Jambot"],
+        typeSpeed: 0,
+        showCursor: false
+      });
+       
+      $('#btnholder').html("<input type='button' class='btn' id='replaybutton' onclick='javascript:void();' value='Replay' />")
+      $('#replaybutton').on('click', function() {
+        play() 
+      });
+    });
+  }
+});
+
